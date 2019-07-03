cb3f51365fc82aefaae4f64344b3c76490e2551b
Vladimir Ventura <vladimir.ventura@libertymutual.com>
2019-07-03 15:52:36 -0400

    Created separate file to keep track of the text displays of the app

---
+++ lib/util/texts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Texts{
+  // App texts
+  static const app_title = 'Teambuilder';
+  static const release = '-ALPHA-';
+  static const appbar_title = Text('Teambuilder');
+  // Tab texts
+
+  // Form texts
+  static const project_name = 'Project Name';
+  static const name_error_msg = 'Please fill in a Project Name';
+  static const project_description = 'Project Description';
+  static const description_error = 'Please fill in a description';
+  static const complexities = [
+    'Beginner', 
+    'Intermediate', 
+    'Expert'
+  ];
+}
