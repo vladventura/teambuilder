5b30d71fb59820bf43ee319978582fff337944dd
Vladimir Ventura <vladimir.ventura@libertymutual.com>
2019-07-03 16:07:24 -0400

    Changing some more stuff

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// To keep a structured and organized, concise code
+// I'll import the texts from the Texts file
+import 'texts.dart';
 
 class Constants {
 
@@ -6,43 +9,51 @@ class Constants {
   // Tabs information
   static const app_tabs = 2;
   static const join_project = {
-    'text': 'Join Project',
+    'text': Texts.join_project,
     'icon': Icon(Icons.search) 
   };
   static const create_project = {
-    'text': 'Create Project',
+    'text': Texts.create_project,
     'icon': Icon(Icons.search)
   };
 
   // Form information
+  static const decoration_width = 2.0;
   static const form_column_margins = EdgeInsets.all(5);
   static const project_name_screen_percent = .60;
   static const description_max_lines = 5;
   static const description_max_length = 500;
   static const has_autocorrect = true;
   static const max_length_enforced = true;
+  static const complexity_padding = EdgeInsets.symmetric(horizontal: 5);
 
   static formDecoration(String formName){
-    const borderDecoWidth = 2.0;
     return InputDecoration(
       labelText: formName,
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(25),
         borderSide: const BorderSide(
           color: main_color,
-          width: borderDecoWidth,
+          width: decoration_width,
         ),
       ),
       focusedBorder: OutlineInputBorder(
         borderSide: BorderSide(
           color: side_color,
-          width: borderDecoWidth,
+          width: decoration_width,
         ),
       ),
     );
   }
-
-
+  static complexitiesDecoration(){
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(7),
+      border: Border.all(
+        color: side_color,
+        width: decoration_width,
+        ),
+    );
+  }
 
   static const items_per_row = 5;
   static const item_size = 5.0;
@@ -50,7 +61,5 @@ class Constants {
   static const side_color = Color (0xFF8BAC0F);
   static const third_color = Color (0xFF306230);
   static const spacing = 3.0;
-
-  static const homescreen_appbar_title = Text ('Main Menu');
   static const appbar_elevation = 2.0;
 }
