b5d52bc05523f8937de56281501942267ca1acdb
Vlad Ventura <vladventura@outlook.es>
2019-07-13 13:34:32 -0400

    Added layout of the app flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:teambuilder/database/dbmanager.dart';
-import 'package:teambuilder/usable/displayform.dart';
-import 'package:teambuilder/usable/displayteams.dart';
-import 'package:teambuilder/util/constants.dart';
-import 'package:teambuilder/util/texts.dart';
 
-void main() => runApp(MyApp());
+import 'package:teambuilder/screens/login.dart';
+import 'package:teambuilder/screens/home.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "${Texts.app_title} ${Texts.release}",
-      home: MyHomepage(),
-    );
-  }
-}
-
-class MyHomepage extends StatefulWidget {
-  @override
-  _MyHomepageState createState() => _MyHomepageState();
-}
-
-class _MyHomepageState extends State<MyHomepage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  ScrollController _scrollController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: Constants.app_tabs, vsync: this);
-    _scrollController = ScrollController();
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: restartDatabase(),
-        resizeToAvoidBottomPadding: false,
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Constants.third_color,
-        drawer: Drawer(
-          child: Text('data'),
-        ),
-        body: NestedScrollView(
-            physics: BouncingScrollPhysics(),
-            controller: _scrollController,
-            headerSliverBuilder: (BuildContext context, bool isScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                    backgroundColor: Constants.main_color,
-                    title: Texts.appbar_title,
-                    forceElevated: isScrolled,
-                    pinned: true,
-                    floating: true,
-                    bottom: TabBar(
-                      controller: _tabController,
-                      tabs: <Widget>[
-                        Tab(
-                            text: Constants.join_project['text'],
-                            icon: Constants.join_project['icon']),
-                        Tab(
-                            text: Constants.create_project['text'],
-                            icon: Constants.create_project['icon'])
-                      ],
-                    ))
-              ];
-            },
-            body: TabBarView(
-              controller: _tabController,
-              children: <Widget>[DisplayTeams(), DisplayForm()],
-            )));
-  }
-
-  restartDatabase() {
-    return FloatingActionButton(
-      child: Icon(Icons.delete),
-      onPressed: () {
-        var dbLink = DBManager();
-        dbLink.deleteProjects();
-        setState(() {
-          DisplayTeams();
-        });
-      },
-    );
-  }
+void main(){
+  runApp(MaterialApp(
+    routes: <String, WidgetBuilder>{
+      '/Start': (context) => MyHomepage(),
+    },
+    home: LoginScreen(),
+  ));
 }
