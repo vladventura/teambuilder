ba6ed430d6d2f184c6ca9c35b150fa66b29c2979
Vlad Ventura <vladventura@outlook.es>
2019-07-12 21:05:36 -0400

    Finished Login Screen layout. WIP: Coloring and functionality needed.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(LoginScreen());
+void main() => runApp(MaterialApp(home: LoginScreen(),));
 
 
 class LoginScreen extends StatefulWidget{
@@ -15,8 +15,66 @@ class _LoginScreenState extends State<LoginScreen>{
   }
 
   Widget build (BuildContext context){
-    return MaterialApp(
-      home: Scaffold(),
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          shrinkWrap: true,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.amber,
+              radius: 48,
+              child: CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 38,
+              ),
+            ),
+            SizedBox(height: 48.0,),
+            // Email
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 8,),
+            // Password
+            TextFormField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30)
+                )
+              ),
+            ),
+            SizedBox(height: 24),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: RaisedButton(
+                onPressed: (){},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.all(12),
+                color: Colors.amber,
+                child: Text('Log In', style: TextStyle(color: Colors.white))
+              ),
+            ),
+            FlatButton(
+              child: Text('Create Account', style: TextStyle(color: Colors.black54),),
+              onPressed: (){},
+            ),
+          ],
+        ),
+      ),
     );
   }
 
