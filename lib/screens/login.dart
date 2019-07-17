7b74ce4c89b3f8c75b46637e183c21db01580646
Vladimir Ventura <vladimir.ventura@libertymutual.com>
2019-07-17 13:57:31 -0400

    Added return statements for good practice

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:teambuilder/database/dbmanager.dart';
-import 'package:teambuilder/models/user.dart';
-
+import 'package:teambuilder/exceptions/userex.dart';
 class LoginScreen extends StatefulWidget{
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen>{
   final _formKey = new GlobalKey<FormState>();
-  String username, email, password;
+  String username, password;
   @override
   void initState(){
     super.initState();
   }
+  /*
+  TODO: True login on the main page
+  TODO: Find a way to return a user object to assign to this instance of the app so we can use the user's info
+  TODO: Change that logo, it looks horrible btw
 
+  First delete unnecessary form fields.
+    Easy. Take away the email one.
+  Then, check if the database has a user with the specified username
+  Then, check if the entry with this name matches the password entered
+    Medium. This should be done server side; the user sends the credentials.
+    Credentials must never ever EVER be checked client side; variables are stored in
+    memory and this means that the user's password would also be stored in memory.
+  Thus, an authenticate page that handles all this should be made.
+  */
   Widget build (BuildContext context){
     return Scaffold(
       body: Center(
@@ -34,12 +47,15 @@ class _LoginScreenState extends State<LoginScreen>{
             SizedBox(height: 48.0,),
             // Username
               TextFormField(
-              onSaved: (username){
-                this.username = username;
-              },
               validator: (username){
+                try{
+                  checkUser(username);
+                } on UserExeption {
+                  return "This username is not registered";
+                }
                 if (username.contains(' ')) return "Usernames must not contain spaces";
                 if (username.isEmpty) return "Required Field.";
+                return null;
               },
               textInputAction: TextInputAction.next,
               autofocus: false,
@@ -51,29 +67,7 @@ class _LoginScreenState extends State<LoginScreen>{
                 ),
               ),
             ),
-            SizedBox(height: 8,),
-            // Email
-            TextFormField(
-              onSaved: (email){
-                this.email = email;
-              },
-              validator: (email){
-                if (email.contains(' ')) return "Emails must not contain spaces";
-                if (!email.contains('@')) return "This is not a valid email.";
-                if (email.isEmpty) return "Required Field.";
-              },
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.emailAddress,
-              autofocus: false,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-            ),
-            SizedBox(height: 8,),
+            SizedBox(height: 8,),        
             // Password
             TextFormField(
               onSaved: (password){
@@ -83,6 +77,7 @@ class _LoginScreenState extends State<LoginScreen>{
                 if (password.contains(' ')) return "Passwords must not contain spaces.";
                 if (password.isEmpty) return "Required field\n";
                 if (password.length < 6) return "Passwords should be larger than 6 characters";
+                return null;
               },
               autofocus: false,
               obscureText: true,
@@ -99,7 +94,7 @@ class _LoginScreenState extends State<LoginScreen>{
               padding: EdgeInsets.symmetric(vertical: 16),
               child: RaisedButton(
                 onPressed: (){
-                submitUser();
+                  loginUser();
                 },
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
@@ -112,6 +107,7 @@ class _LoginScreenState extends State<LoginScreen>{
             FlatButton(
               child: Text('Create Account', style: TextStyle(color: Colors.black54),),
               onPressed: (){
+                Navigator.of(context).pushNamed('/Create');
               },
             ),
           ],
@@ -120,18 +116,17 @@ class _LoginScreenState extends State<LoginScreen>{
       ),
     );
   }
-  void submitUser(){
-    var dbLink = DBManager();
-    if(this._formKey.currentState.validate()){
-      _formKey.currentState.save();
+
+  loginUser(){
+    if(_formKey.currentState.validate()) Navigator.of(context).pushNamedAndRemoveUntil('/Home', (Route <dynamic> route) => false);
+  }
+
+  checkUser(String username){
+    var dbLink = new DBManager();
+    try{
+      dbLink.authenticate(username, "username");
+    } on DatabaseException{
+      throw new UserExeption('user');
     }
-    else 
-    return null;
-    User user = new User();
-    user.username = username;
-    user.email = email;
-    user.password = password;
-    dbLink.insertUser(user);
-    Navigator.of(context).pushNamedAndRemoveUntil('/Home', (Route <dynamic> route) => false);
   }
 }
