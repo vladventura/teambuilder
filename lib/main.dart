4693e8c288d05de6a9df2b5e3acfca2cf49114c2
Vladimir Ventura <vladimir.ventura@libertymutual.com>
2019-07-17 13:58:14 -0400

    Added route for the create account page of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:teambuilder/screens/create.dart';
 
 import 'package:teambuilder/screens/login.dart';
 import 'package:teambuilder/screens/home.dart';
+
+/* 
+TODO: Set PRs for all the TODOs (:
+TODO: Set a proper color scheme for the app, like a palette of something
+TODO: Firebase integration
+*/
 
 void main(){
   runApp(MaterialApp(
     routes: <String, WidgetBuilder>{
       '/Home': (context) => MyHomepage(),
+      '/Create': (context) => CreateScreen(),
     },
     home: LoginScreen(),
   ));
