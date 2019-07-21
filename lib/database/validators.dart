2359030453c4f804cb02bc29f45d136991331857
Vlad Ventura <vladventura@outlook.es>
2019-07-21 16:46:13 -0400

    Merging the test's results with the main project

--- lib/database/validators.dart
+++ lib/database/validators.dart
@@ -1,4 +1,17 @@
+/* 
+  This is a two function file. 
+  It enumerates the kinds of forms that could happen, which renders my create page useless.
+  It also holds the validators for each form field.
+*/
+
 enum FormType {login, register}
+
+class UsernameValidator{
+  static String validate(String value){
+    if (value.isEmpty) return "Username may not be empty";
+    return null;
+  }
+}
 
 class EmailValidator{
   static String validate(String value){
