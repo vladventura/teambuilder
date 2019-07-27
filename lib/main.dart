08b23ac359a6e6432f637486f17ef220d928db57
Vladimir Ventura <vladimir.ventura@libertymutual.com>
2019-07-27 12:04:08 -0400

    I don't know what I'm doing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teambuilder/screens/futurebuilderhome.dart';
 
 import 'package:teambuilder/screens/login.dart';
 import 'package:teambuilder/screens/home.dart';
@@ -11,7 +12,7 @@ TODO: Set a proper color scheme for the app, like a palette of something
 void main() {
   runApp(MaterialApp(
     routes: <String, WidgetBuilder>{
-      '/Home': (context) => MainScreen(),
+      '/Home': (context) => FutureMainScreen(),
     },
     home: Login(),
   ));
