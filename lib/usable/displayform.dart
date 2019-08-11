4fce040d10910ed12c5120226edfa20d96a96d8f
Vladimir Ventura <vladventura@outlook.es>
2019-08-11 12:33:50 -0400

    Currently breaking down the form a bit so its easier to read and understand.

--- lib/usable/displayform.dart
+++ lib/usable/displayform.dart
@@ -1,9 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 // Constant values and texts
 import 'package:teambuilder/util/constants.dart';
 import 'package:teambuilder/util/texts.dart';
+import 'package:teambuilder/util/validators.dart';
 
 class DisplayForm extends StatefulWidget {
   @override
@@ -11,16 +11,10 @@ class DisplayForm extends StatefulWidget {
 }
 
 class _DisplayFormState extends State<DisplayForm> {
-  // This is to display and get the value from the
-  // Dropdown list
-  String _complexity;
-  List<String> complexities = new List<String>();
-  // This is to handle the data from the form
-  String name, description, contactPlatforms;
-  // This is a needed key. The main scaffold also needs one
-  final _formKey = new GlobalKey<FormState>();
-
   FirebaseUser _user;
+  String _complexity, _name, _description, _contactPlatforms;
+  List<String> complexities = new List<String>();
+  final _formKey = new GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -29,7 +23,7 @@ class _DisplayFormState extends State<DisplayForm> {
     complexities.addAll(Texts.complexities);
   }
 
-  Future<dynamic> asyncSetup() async{
+  Future<dynamic> asyncSetup() async {
     _user = await FirebaseAuth.instance.currentUser();
   }
 
@@ -41,73 +35,73 @@ class _DisplayFormState extends State<DisplayForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        top: false,
-        child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: Constants.form_column_margins,
-                  width: MediaQuery.of(context).size.width *
-                      Constants
-                          .project_name_screen_percent, // Reactive screen size
-                  child: TextFormField(
-                      textCapitalization: TextCapitalization.sentences,
-                      textInputAction: TextInputAction.go,
-                      autocorrect: Constants.has_autocorrect,
-                      onSaved: (name) {
-                        this.name = name;
-                      },
-                      validator: (name) {
-                        if (name.isEmpty) return Texts.name_error_msg;
-                        return null;
-                      },
-                      decoration: Constants.formDecoration(Texts.project_name)),
-                ),
-                Container(
-                  margin: Constants.form_column_margins,
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                      maxLines: Constants.description_max_lines,
-                      maxLength: Constants.description_max_length,
-                      maxLengthEnforced: Constants.max_length_enforced,
-                      autocorrect: Constants.has_autocorrect,
-                      validator: (description) {
-                        if (description.isEmpty) return Texts.description_error;
-                        return null;
-                      },
-                      onSaved: (description) => this.description = description,
-                      decoration:
-                          Constants.formDecoration(Texts.project_description)),
-                ),
-                Container(
-                    margin: Constants.form_column_margins,
-                    padding: Constants.complexity_padding,
-                    decoration: Constants.complexitiesDecoration(),
-                    child: DropdownButtonHideUnderline(
-                        child: DropdownButton(
-                            hint: Texts.project_complexity_text,
-                            value: _complexity,
-                            onChanged: (String value) {
-                              _onChanged(value);
-                            },
-                            items: complexities.map((String value) {
-                              return new DropdownMenuItem(
-                                  value: value, child: Text(value));
-                            }).toList()))),
-                Container(
-                    margin: Constants.complexity_padding,
-                    child: Center(
-                        child: FlatButton(
-                      color: Colors.transparent,
-                      child: Icon(Icons.add),
-                      onPressed: (() {
-                        //submitProject();
-                      }),
-                    )))
-              ],
-            )));
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[],
+      ),
+    );
+  }
+
+  Container buildNameBox() {
+    return Container(
+      margin: Constants.form_column_margins,
+      width: MediaQuery.of(context).size.width *
+          Constants.project_name_screen_percent,
+      child: TextFormField(
+        textCapitalization: TextCapitalization.sentences,
+        textInputAction: TextInputAction.next,
+        autocorrect: Constants.has_autocorrect,
+        decoration: Constants.formDecoration(Texts.project_name),
+        onSaved: (name) {
+          this._name = name;
+        },
+        validator: ProjectNameValidator.validate,
+      ),
+    );
+  }
+
+  Container buildDescriptionBox() {
+    return Container(
+      margin: Constants.form_column_margins,
+      width: MediaQuery.of(context).size.width,
+      child: TextFormField(
+          maxLines: Constants.description_max_lines,
+          maxLength: Constants.description_max_length,
+          maxLengthEnforced: Constants.max_length_enforced,
+          autocorrect: Constants.has_autocorrect,
+          validator: ProjectDescriptionValidator.validate,
+          onSaved: (description) => this._description = description,
+          decoration: Constants.formDecoration(Texts.project_description)),
+    );
+  }
+
+  Container buildComplexityDropdow() {
+    return Container(
+        margin: Constants.form_column_margins,
+        padding: Constants.complexity_padding,
+        decoration: Constants.complexitiesDecoration(),
+        child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+                hint: Texts.project_complexity_text,
+                value: _complexity,
+                onChanged: (String value) => _onChanged(value),
+                items: complexities.map((String value) {
+                  return new DropdownMenuItem(value: value, child: Text(value));
+                }).toList())));
+  }
+
+  Container buildSubmitButton() {
+    return Container(
+        margin: Constants.complexity_padding,
+        child: Center(
+            child: FlatButton(
+          color: Colors.transparent,
+          child: Icon(Icons.add),
+          onPressed: (() {
+            //submitProject();
+          }),
+        )));
   }
 }
