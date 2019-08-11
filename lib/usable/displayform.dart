ca547c5821e59e94e9fb059f62ea840653baee04
Vladimir Ventura <vladventura@outlook.es>
2019-08-11 14:09:00 -0400

    Added function for the onClick of the submit button.

--- lib/usable/displayform.dart
+++ lib/usable/displayform.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:teambuilder/models/project.dart';
 // Constant values and texts
 import 'package:teambuilder/util/constants.dart';
 import 'package:teambuilder/util/texts.dart';
@@ -11,20 +13,16 @@ class DisplayForm extends StatefulWidget {
 }
 
 class _DisplayFormState extends State<DisplayForm> {
-  FirebaseUser _user;
   String _complexity, _name, _description, _contactPlatforms;
   List<String> complexities = new List<String>();
+  final _auth = FirebaseAuth.instance;
   final _formKey = new GlobalKey<FormState>();
+  final db = Firestore.instance;
 
   @override
   void initState() {
     super.initState();
-    asyncSetup();
     complexities.addAll(Texts.complexities);
-  }
-
-  Future<dynamic> asyncSetup() async {
-    _user = await FirebaseAuth.instance.currentUser();
   }
 
   void _onChanged(String value) {
@@ -99,9 +97,26 @@ class _DisplayFormState extends State<DisplayForm> {
             child: FlatButton(
           color: Colors.transparent,
           child: Icon(Icons.add),
-          onPressed: (() {
+          onPressed: (() async {
             //submitProject();
           }),
         )));
+  }
+
+  void submitProject() async{
+    if (_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      FirebaseUser _user = await _auth.currentUser();
+      CollectionReference reference  = db.collection('projects');
+      Project project = new Project(
+        _user.uid, 
+        _name, 
+        _description, 
+        _complexity, 
+        _contactPlatforms, 
+        _user.displayName
+      );
+      reference.add(project.toMap());
+    }
   }
 }
