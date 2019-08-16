41ba216a3f8421bead179bde3eeb85151796858b
Vladimir Ventura <vladventura@outlook.es>
2019-08-16 19:45:34 -0400

    Removed unused database parameters

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -63,24 +63,6 @@ class Constants {
       )
     );
   }
- 
-  // Database information
-  static const database_filename = 'projects.db';
-  static const projects_query_name = 'projects';
-  static const users_query_name = 'users';
-  static const database_version = 5;
-  static const on_create_SQL = 
-    """
-    CREATE TABLE $projects_query_name(id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT, description TEXT, complexity TEXT, contactPlatforms TEXT);
-    """;
-  // Version 5
-  static const on_update_SQL =
-  """
-  ALTER TABLE $projects_query_name ADD COLUMN originator TEXT;
-  """;
-
-  static const select_all_from_db = """SELECT * FROM projects;""";
 
   // General information
   static const items_per_row = 5;
