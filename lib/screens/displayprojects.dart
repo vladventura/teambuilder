618de9519b77a6216b823e9933a7a558bf523186
Vlad Ventura <vladventura@outlook.es>
2019-09-13 14:10:48 -0400

    This branch resolves issue #21. I did some research in a package that allowed me to display toast messages, and after dealing with Flushbar, Flash seems to be doing the trick.

--- lib/screens/displayprojects.dart
+++ lib/screens/displayprojects.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:teambuilder/util/constants.dart';
 
+import 'package:flash/flash.dart';
 class DisplayProjects extends StatefulWidget {
   _DisplayProjectsState createState() => _DisplayProjectsState();
 }
