501d3192cfef8189f8a51705f7a6ab70cdb6de80
Vlad Ventura <vladventura@outlook.es>
2019-09-14 12:05:33 -0400

    Created a function similar to the one that has the decoration for the textboxes, but this one takes an IconButton because I found no way to send a setState function as a function argument

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -45,6 +45,26 @@ class Constants {
       ),
     );
   }
+    static dynamicFormDecoration (String formName, IconButton ib){
+    return InputDecoration(
+      hintText: formName,
+      hintStyle: TextStyle(
+        color: generalTextColor,
+      ),
+      enabledBorder: new OutlineInputBorder(
+        borderRadius: new BorderRadius.circular(20),
+        borderSide: const BorderSide(
+          color: formInactiveColor,
+        ),
+      ),
+      focusedBorder: new OutlineInputBorder(
+        borderSide: new BorderSide(
+          color: formActiveColor,
+        ),
+      ),
+      suffixIcon: ib,
+    );
+  }
   static complexitiesDecoration(){
     return BoxDecoration(
       borderRadius: BorderRadius.circular(7),
