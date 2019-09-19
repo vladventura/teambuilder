4737f8697bdb3f06a632e9cbbc0681963c1e32e0
Vladimir Ventura <vladventura@outlook.es>
2019-09-18 23:53:29 -0400

    Even more linting to this file

--- lib/screens/displayprojects.dart
+++ lib/screens/displayprojects.dart
@@ -1,4 +1,5 @@
 library project;
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash/flash.dart';
